9305a067da99c0828cae1651cf5648183f201e93
MAC9572 <jibinjoel123@gmail.com>
2024-09-07 16:33:28 +0530

    All files added to commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let x=4;
+function checkOddOrEven(x) {
+    if (x %2 === 0) {
+        return "Number is even";
+    }
+    else {
+    return "Number is odd";
+}
+}
+console.log(checkOddOrEven(x));
